0d66306deaba3e9dcdfa7a4b58b707b1429472f2
zach <zach@web3technologies.io>
2023-09-01 14:21:47 -0500

    create fetch user data task and fetching abstraction

---
+++ src/friend_trader_dispatcher/tasks/user_actions.py
@@ -0,0 +1,26 @@
+from celery import shared_task
+from .base import BaseCeleryTask
+from requests import HTTPError
+
+from friend_trader_trader.models import FriendTechUser
+
+
+@shared_task(
+    bind=True, 
+    name="retrieve_users_data", 
+    base=BaseCeleryTask,
+    # autoretry_for=(, ),
+    retry_backoff=15,
+    max_retries=3
+    )
+def retrieve_users_data(self, friend_tech_user_data:list):
+    users_to_update = []
+    for friend_tech_user in friend_tech_user_data:
+        f_user = FriendTechUser.objects.get(address=friend_tech_user.get("address"))
+        try:
+            f_user.get_kossetto_data(auto_save=False)
+            users_to_update.append(f_user)
+        except HTTPError as e:
+            return f"{self.address} not found in kossetto"
+    FriendTechUser.objects.bulk_update(users_to_update, fields=["twitter_username", "twitter_profile_pic"])
+    return friend_tech_user_data
--- src/friend_trader_trader/models/friend_tech_user.py
+++ src/friend_trader_trader/models/friend_tech_user.py
@@ -1,5 +1,8 @@
 from django.db import models
 
+from friend_trader_core.clients import kossetto_client
+from requests import Timeout
+
 
 class FriendTechUser(models.Model):
     
@@ -9,4 +12,18 @@ class FriendTechUser(models.Model):
     twitter_profile_pic = models.CharField(max_length=255, null=True, default=None)
     twitter_followers = models.IntegerField(null=True, default=None)
     share_count = models.IntegerField(null=True, default=None)
-    
+
+
+    def get_kossetto_data(self, auto_save=True):
+        try:
+            kossetto_data = kossetto_client.get_kossetto_user(address=self.address)
+            twitter_username = kossetto_data.get("twitterUsername")
+            profile_pic_url = kossetto_data.get("twitterPfpUrl")
+            self.twitter_username=twitter_username
+            self.twitter_profile_pic=profile_pic_url
+            if auto_save:
+                self.save(update_fields=["twitter_username", "twitter_profile_pic"])
+            return self
+        except Timeout as e:
+            print("handle timeout logic")
+        
