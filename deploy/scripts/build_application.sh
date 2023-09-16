32141f2db4887662a209045a1045a3434a493d16
= <=>
2023-09-16 13:42:49 -0500

    update build sh

--- deploy/scripts/build_application.sh
+++ deploy/scripts/build_application.sh
@@ -35,7 +35,7 @@ pip install $jenkins_proj_path .
 pip install wheel
 python setup.py bdist_wheel 
 deactivate
-echo "*** Purse Backend Module Created***"
+echo "*** FriendTrader Backend Module Created***"
 
 echo "Building the application"
 application_build_path=/applications/friend_trader.tar
