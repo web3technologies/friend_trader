ae5affbc1815322873771f25b3f5a381cbc6333e
zach <zach@web3technologies.io>
2023-09-16 14:34:25 -0500

    add dev settings

--- src/friend_trader/settings/dev.py
+++ src/friend_trader/settings/dev.py
@@ -5,3 +5,14 @@ CORS_ALLOW_ALL_ORIGINS = False
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
 ]
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'friend_trader',
+        'USER': 'friend_trader',
+        'PASSWORD': "Testing321.",
+        'HOST': "localhost",
+        'PORT': '5432',
+    }
+}
