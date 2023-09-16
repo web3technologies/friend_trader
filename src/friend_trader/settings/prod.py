ee150432290accdf68d79e1735aa966f1a0c776b
zach <zach@web3technologies.io>
2023-09-16 15:37:22 -0500

    add static root

--- src/friend_trader/settings/prod.py
+++ src/friend_trader/settings/prod.py
@@ -5,3 +5,8 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "https://friendtrader.tech"
 ]
+
+MEDIA_ROOT = "/applications/friend_trader/"
+STATIC_ROOT = f"/applications/friend_trader/static/"
+
+TEMPLATES[0]['DIRS'] = [STATIC_ROOT]
