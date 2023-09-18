04c5e6c21a831176d61289f01bdae075e2725b6b
= <=>
2023-09-17 19:39:38 -0500

    add configuration and deploy for celery and websocket

--- src/friend_trader/settings/prod.py
+++ src/friend_trader/settings/prod.py
@@ -1,7 +1,6 @@
 from .settings import *
 
 
-
 ALLOWED_HOSTS = ["friendtrader.tech", "backend.friendtrader.tech"]
 
 CORS_ALLOW_ALL_ORIGINS = False
@@ -15,3 +14,20 @@ STATIC_ROOT = f"/applications/friend_trader/static/"
 TEMPLATES[0]['DIRS'] = [STATIC_ROOT]
 
 DEBUG = False
+
+
+CELERY_BROKER_URL = "sqs://"
+CELERY_TASK_DEFAULT_QUEUE = 'friend_trader'
+
+
+CELERY_BROKER_TRANSPORT_OPTIONS = {
+    'region': 'us-east-2',
+    'is_secure': True,
+    'predefined_queues': {
+        'friend_trader': {
+            'url': 'https://sqs.us-east-2.amazonaws.com/490305332793/friend_trader',
+        },
+    },
+    'visibility_timeout': 3600,
+    'polling_interval': 5.0,
+}
