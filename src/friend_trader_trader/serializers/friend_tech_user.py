8e2ea71c3b429d9cc4e483398868bbbdd7b1f050
= <=>
2023-09-26 21:06:42 -0500

    add time

--- src/friend_trader_trader/serializers/friend_tech_user.py
+++ src/friend_trader_trader/serializers/friend_tech_user.py
@@ -29,11 +29,18 @@ class FriendTechUserListSerializer(serializers.ModelSerializer):
 class FriendTechUserListLatestPriceSerializer(serializers.ModelSerializer):
     
     latest_price = PriceSerializer(read_only=True)
+    last_trade_time = serializers.SerializerMethodField("last_trade_time")
     
     class Meta:
         model = FriendTechUser
         fields = ("id", "twitter_username", "twitter_profile_pic", "twitter_followers", "latest_price", "shares_supply")
-        
+    
+    def last_trade_time(self, obj, *args, **kwargs):
+        if last_trade := obj.share_prices.select_related("block").order_by("block__block_timestamp").last():
+            return last_trade.block.block_timestamp
+        else:
+            return None
+    
 
 class FriendTechUserCandleStickSerializer(FriendTechUserSerializer):
     
