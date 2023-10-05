f3c17797a88da0035aa611072346bbb11ed1ebc1
= <=>
2023-10-04 20:11:10 -0500

    add percent change to seriazlier

--- src/friend_trader_trader/serializers/friend_tech_user.py
+++ src/friend_trader_trader/serializers/friend_tech_user.py
@@ -1,8 +1,9 @@
+from django.utils import timezone
 
 from rest_framework import serializers
 
 from friend_trader_core.utils import convert_to_central_time
-from friend_trader_trader.models import FriendTechUser
+from friend_trader_trader.models import FriendTechUser, Price
 from friend_trader_trader.serializers.price import PriceSerializer
 
 
@@ -30,16 +31,53 @@ class FriendTechUserListLatestPriceSerializer(serializers.ModelSerializer):
     
     latest_price = PriceSerializer(read_only=True)
     last_trade_time = serializers.SerializerMethodField("get_last_trade_time")
+    twenty_four_hour_percent_change = serializers.SerializerMethodField("get_twenty_four_hour_percent_change")
+    seven_day_percent_change = serializers.SerializerMethodField("get_seven_day_percent_change")
     
     class Meta:
         model = FriendTechUser
-        fields = ("id", "twitter_username", "twitter_profile_pic", "twitter_followers", "latest_price", "shares_supply", "last_trade_time")
+        fields = ("id", "twitter_username", "twitter_profile_pic", "twitter_followers", "latest_price", "shares_supply", "last_trade_time", "twenty_four_hour_percent_change", "seven_day_percent_change")
     
     def get_last_trade_time(self, obj, *args, **kwargs):
         if last_trade := obj.share_prices.select_related("block").order_by("block__block_timestamp").last():
             return last_trade.block.block_timestamp
         else:
             return None
+        
+    def get_twenty_four_hour_percent_change(self, obj, *args, **kwargs):
+        twenty_four_hours_ago = timezone.now() - timezone.timedelta(days=1)
+        twenty_four_hours_ago_unix = int(twenty_four_hours_ago.timestamp())
+
+        first_price_since_24_hours_ago = Price.objects.filter(
+            trade__subject=obj, 
+            trade__block__block_timestamp__gte=twenty_four_hours_ago_unix
+        ).order_by("trade__block__block_timestamp").first()
+
+        if not first_price_since_24_hours_ago:
+            return 0
+         
+        if obj.latest_price.price == 0:
+            return 0
+        percent_change = (1 - (first_price_since_24_hours_ago.price / obj.latest_price.price)) * 100
+        return percent_change
+        
+    def get_seven_day_percent_change(self, obj, *args, **kwargs):
+        seven_days_ago = timezone.now() - timezone.timedelta(days=7)
+        seven_days_ago_unix = int(seven_days_ago.timestamp())
+        first_price_since_seven_days_ago = Price.objects.filter(
+            trade__subject=obj, 
+            trade__block__block_timestamp__gte=seven_days_ago_unix
+        ).order_by("trade__block__block_timestamp").first()
+        
+        if not first_price_since_seven_days_ago:
+            return 0 
+
+        if obj.latest_price.price == 0:
+            return 0
+
+        percent_change = (1 - (first_price_since_seven_days_ago.price / obj.latest_price.price)) * 100
+
+        return percent_change
     
 
 class FriendTechUserCandleStickSerializer(FriendTechUserSerializer):
