9ffefd299531eb17d4e77f1f90803900734e5307
= <=>
2023-10-06 16:28:03 -0500

    update loging lifetime

--- src/friend_trader/settings/settings.py
+++ src/friend_trader/settings/settings.py
@@ -89,7 +89,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60*2),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
     'AUTH_HEADER_TYPES': ('Bearer',)
 }
