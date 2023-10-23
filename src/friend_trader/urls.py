5107b20ce9e4fd1522e30a4f2809dcfb87c8f156
zach <zach@web3technologies.io>
2023-10-22 19:14:32 -0500

    add back auth app

--- src/friend_trader/urls.py
+++ src/friend_trader/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('friend-trader/', include("friend_trader_trader.urls")),
-    # path("auth/", include("friend_trader_auth.urls"))
+    path("auth/", include("friend_trader_auth.urls"))
 ]
